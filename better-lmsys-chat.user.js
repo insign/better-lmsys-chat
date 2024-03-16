b1f8ff5e261a6107e3568226748ad2cec07cc895
Hélio <insign@gmail.com>
2024-03-15 23:16:20 -0300

    small fix

--- better-lmsys-chat.user.js
+++ better-lmsys-chat.user.js
@@ -1,16 +1,16 @@
-//  ==UserScript==
-//  @name         Better LMSYS Chat
-//  @namespace    https://github.com/insign/better-lmsys-chat
-//  @version      202403152308
-//  @description  make chat lmsys chat better and clean
-//  @match        https://chat.lmsys.org
-//  @icon         https://www.google.com/s2/favicons?sz=64&domain=lmsys.org
-//  @author       Hélio <insign@gmail.com>
-//  @copyright    Hélio <insign@gmail.com>
-//  @license      WTFPL
-//  @downloadURL  https://update.greasyfork.org/scripts/489922/Better%20LMSYS%20Chat.user.js
-//  @updateURL    https://update.greasyfork.org/scripts/489922/Better%20LMSYS%20Chat.meta.js
-//  ==/UserScript==
+// ==UserScript==
+// @name Better LMSYS Chat
+// @namespace https://github.com/insign/better-lmsys-chat
+// @version 202403152308
+// @description make chat lmsys chat better and clean
+// @match https://chat.lmsys.org
+// @icon https://www.google.com/s2/favicons?sz=64&domain=lmsys.org
+// @author Hélio <insign@gmail.com>
+// @copyright Hélio <insign@gmail.com>
+// @license WTFPL
+// @downloadURL https://update.greasyfork.org/scripts/489922/Better%20LMSYS%20Chat.user.js
+// @updateURL https://update.greasyfork.org/scripts/489922/Better%20LMSYS%20Chat.meta.js
+// ==/UserScript==
 
 (function() {
   'use strict'
