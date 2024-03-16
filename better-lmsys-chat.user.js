44d4059c6689b5a0ec63d9ba20d4ba868d5885f6
Hélio <insign@gmail.com>
2024-03-16 15:04:11 -0300

    @match

--- better-lmsys-chat.user.js
+++ better-lmsys-chat.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/insign/better-lmsys-chat
 // @version 202403152308
 // @description make chat lmsys chat better and clean
-// @match https://chat.lmsys.org
+// @match https://chat.lmsys.org/*
 // @icon https://www.google.com/s2/favicons?sz=64&domain=lmsys.org
 // @author Hélio <insign@gmail.com>
 // @copyright Hélio <insign@gmail.com>
